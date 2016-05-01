bff3f6d37ccdc8ad0eb4dad476dd6515ad9a041f
atowneND <atowne@nd.edu>
2016-04-30 21:47:50 -0400

    it will connect to the student machine

--- client_connection.py
+++ client_connection.py
@@ -14,23 +14,26 @@ port1 = 40084
 port2 = 40092
 
 class ClientConnection(Protocol):
-    def __init__(self):
+    def __init__(self, port):
         self.delimiter = "\r\n\r\n"
+        self.port = port
 
     def lineReceived(self, data):
         print "received data:", data
 
     def connectionMade(self):
-        print "new connection made to", host, "port", port
+        print "new connection made to", host, "port", self.port
 
     def connectionLost(self, reason):
-        print "lost connection to", host, "port", port
+        print "lost connection to", host, "port", self.port
 
 class ClientConnectionFactory(ClientFactory):
+    def __init__(self, port):
+        self.port = port
     protocol = ClientConnection
 
     def buildProtocol(self, addr):
-        return ClientConnection()
+        return ClientConnection(self.port)
 
 class GameConnection:
     def __init__(self):
@@ -39,9 +42,11 @@ class GameConnection:
         lc.start(0.1)
 
         try:
-            reactor.connectTCP(host, port1, ClientConnectionFactory())
+            print "connecting to",port1
+            reactor.connectTCP(host, port1, ClientConnectionFactory(port1))
         except socket.error:
-            reactor.connectTCP(host, port2, ClientConnectionFactory())
+            print "connecting to",port2
+            reactor.connectTCP(host, port2, ClientConnectionFactory(port2))
         except:
             print "Unable to connect"
             exit()
