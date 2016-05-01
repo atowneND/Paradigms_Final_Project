b4784e90fcca44c8f50c789bc722bd73a83bcc91
atowneND <atowne@nd.edu>
2016-04-30 22:11:37 -0400

    quits cleanly

--- gamespace.py
+++ gamespace.py
@@ -23,21 +23,19 @@ class GameSpace:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 reactor.stop()
-                try:
-                    pygame.quit()
-                except pygame.error, message:
-                    pass
+                pygame.quit()
                 try:
                     sys.exit()
                 except BaseException:
                     pass
+                break
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 # http://stackoverflow.com/questions/12150957/pygame-action-when-mouse-click-on-rect
                 self.mouse_pos = pygame.mouse.get_pos()
-        self.test_Ship(self.ship)
-        #self.test_main_menu()
-        pygame.display.flip()
-        self.mouse_pos = (0,0)
+            self.test_Ship(self.ship)
+            #self.test_main_menu()
+            pygame.display.flip()
+            self.mouse_pos = (0,0)
 
     def test_main_menu(self):
         csvfile = "main_menu.csv"
