577a6b487284c432afb39b944e487ed873782d42
atowneND <atowne@nd.edu>
2016-05-04 00:22:28 -0400

    shoots laser projectiles

--- ship.py
+++ ship.py
@@ -48,12 +48,12 @@ class Ship(pygame.sprite.Sprite):
         # shields
         self.shields = 3
         self.currentShield = 3
+        # health
+        self.health = 20
         # weapons
         weapon_name = "ion2.png"
         weapon_dir = "weapons/"+weapon_name
         self.weapon = Weapons(self, weapon_dir)
-        # health
-        self.health = 20
         # crew?
 
     def tick(self):
@@ -62,15 +62,15 @@ class Ship(pygame.sprite.Sprite):
         for i in range(0, self.health):
             healthRect = pygame.Rect(self.rect.x + i*20, self.rect.y - 40, 15, 15)
             pygame.draw.rect(self.gs.screen, (0, 255, 0), healthRect, 0)
-        
+
         # Shield Icons
         for i in range(0, self.shields):
             width = 0
             if i >= self.currentShield:
                 width = 2
             shieldCircle = pygame.draw.circle(self.gs.screen, (0, 0, 255), (self.rect.x + i*30 + 10, self.rect.y - 10), 10, width)
-        
-        # Shield 
+
+        # Shield
         if self.currentShield != 0:
             angle1 = angle2 = 0
             if int(self.player) == 1:
@@ -80,7 +80,7 @@ class Ship(pygame.sprite.Sprite):
                 angle1 = math.pi*0.5
                 angle2 = math.pi*1.5
             pygame.draw.arc(self.gs.screen, (0,0,255), self.shield_rect, angle1, angle2, self.currentShield)
-        
+
         self.grid.tick()
         self.weapon.tick()
 
--- weapons.py
+++ weapons.py
@@ -4,6 +4,7 @@ class Weapons(pygame.sprite.Sprite):
     def __init__(self, ship, img):
         self.gs = ship.gs
         self.ship_player = int(ship.player)
+        self.ship = ship
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
 
@@ -14,32 +15,55 @@ class Weapons(pygame.sprite.Sprite):
         self.firing_enabled = False
         self.target = self.origin
 
-        self.duration = 400
         self.ctr = 0
 
     def tick(self):
         color = (255,50,0)
         self.gs.screen.blit(self.image, self.rect)
+        # firing ship is the player
         if (self.ship_player==int(self.gs.player)):
             if self.rect.collidepoint(self.gs.mouse_pos):
                 self.firing_enabled = True
             if (self.firing_enabled==True) and (self.target != self.origin):
                 start = self.origin
                 end = self.target
-                width = 4
-                pygame.draw.line(self.gs.screen, color, start, end, width)
-                self.ctr += 1
-                if self.ctr > self.duration:
-                    self.firing_enabled = False
-                    self.ctr = 0
-                    self.target = self.origin
+
+                if self.ctr==0:
+                    self.laser = LaserBeam(self.gs,start, end, self)
+                    self.ctr = 1
+                self.laser.tick()
+
+        # firing ship is opponent
         elif (self.ship_player!=int(self.gs.player)) and (self.firing_enabled == True):
             start = self.origin
             end = self.target
-            width = 4
-            pygame.draw.line(self.gs.screen, color, start, end, width)
-            self.ctr += 1
-            if self.ctr > self.duration:
-                self.firing_enabled = False
-                self.ctr = 0
-                self.target = self.origin
+
+            if self.ctr==0:
+                self.laser = LaserBeam(self.gs,start, end, self)
+                self.ctr = 1
+            self.laser.tick()
+
+class LaserBeam(pygame.sprite.Sprite):
+    def __init__(self, gs, start, end, weapon):
+        self.gs = gs
+        self.damage = 1
+        self.start = start
+        self.end = end
+        self.dx = end[0]-start[0]
+        self.dy = end[1]-start[1]
+        width = 50
+        self.target = (self.end[0] - width/2., self.end[1] - width/2., width, width)
+        self.speed = 50
+        self.nextpoint = (self.start[0] + self.dx/self.speed, self.start[1] + self.dy/self.speed)
+
+        self.weapon = weapon
+
+    def tick(self):
+        self.nextpoint = (self.start[0] + self.dx/self.speed, self.start[1] + self.dy/self.speed)
+        pygame.draw.line(self.gs.screen, (255,0,0), self.start, self.nextpoint, 12)
+        self.start = (self.start[0] + self.dx/(8*self.speed), self.start[1] + self.dy/(8*self.speed))
+        if pygame.Rect(self.target).collidepoint(self.nextpoint):
+            self.weapon.firing_enabled = False
+            self.weapon.target = self.weapon.origin
+            self.weapon.ctr = 0
+            #self.weapon.enemy_ship.health = self.weapon.enemy_ship.health -5
